8d74a8878eef2f64b05dcf3565c8d374c170f479
fdbva <villela.felipe@gmail.com>
2021-07-23 21:10:28 -0300

    [Step1] Added references between projects and added AutorModel

---
+++ Domain.Model/Models/AutorModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.Model.Models
+{
+    public class AutorModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string UltimoNome { get; set; }
+        public string Nacionalidade { get; set; }
+        public int QuantidadeLivrosPublicados { get; set; }
+        public DateTime Nascimento { get; set; }
+    }
+}
